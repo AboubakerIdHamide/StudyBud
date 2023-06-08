9341d2ab64e7bdfeaf2b7a3d7ef4b3d77d046f4b
AboubakerIdHamide <aboubakeridbsn@gmail.com>
2023-06-08 10:03:14 +0100

    Dealing with Templates

--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+rooms=[
+    {"id":1, "name":"Learn Python"},
+    {"id":2, "name":"Learn PHP"},
+    {"id":3, "name":"Learn Django"},
+    {"id":4, "name":"Learn Laravel"},
+]
 
 def home(req):
-    return HttpResponse('home')
+    data={"rooms":rooms}
+    return render(req, "base/home.html", data)
 
-def room(req):
-    return HttpResponse('room')
+def room(req, pk):
+    matchedRoom=None
+    for i in rooms:
+        if i["id"]==int(pk):
+            matchedRoom=i
+    data={"room":matchedRoom}
+    return render(req, "base/room.html", data)
