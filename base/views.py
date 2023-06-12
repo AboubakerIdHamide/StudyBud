e39e1862ab527b753437d1ec90e2c276b95cf397
AboubakerIdHamide <aboubakeridbsn@gmail.com>
2023-06-12 19:28:42 +0100

    Django CRUD & Model Forms (Magic)

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomFrom
 
 def home(req):
     data={"rooms": Room.objects.all()}
@@ -8,3 +9,37 @@ def home(req):
 def room(req, pk):
     data={"room": Room.objects.get(id=pk)}
     return render(req, "base/room.html", data)
+
+def createRoom(req):
+    if req.method=="POST":
+        form= RoomFrom(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    data={ 'form' : RoomFrom() }
+    return render(req, "base/room_form.html", data)
+
+def updateRoom(req, pk):
+    room= Room.objects.get(id=pk)
+    form= RoomFrom(instance=room)
+
+    if req.method=="POST":
+        form= RoomFrom(req.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    data={ 'form' : form }
+    return render(req, "base/room_form.html", data)
+
+def deleteRoom(req, pk):
+    room= Room.objects.get(id=pk)
+
+    if req.method=="POST":
+        room.delete()
+        return redirect('home')
+    
+    data={'obj':room}
+    return render(req, "base/delete.html", data)
+
